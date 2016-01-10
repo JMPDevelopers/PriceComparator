71c2fda9a9af52a23d89da31cbfad56959de84db
ja3ekja <jacekfeliksiak@gmail.com>
2016-01-10 18:00:03 +0100

    Fill pom, create Application class and controller and .gitignore file

--- PriceComparator/src/main/java/com/jmpdev/Application.java
+++ PriceComparator/src/main/java/com/jmpdev/Application.java
@@ -1,7 +1,15 @@
 package com.jmpdev;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by jacek on 10.01.16.
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(Application.class, args);
+    }
 }
--- PriceComparator/src/main/java/com/jmpdev/controllers/HelloController.java
+++ PriceComparator/src/main/java/com/jmpdev/controllers/HelloController.java
@@ -1,7 +1,18 @@
 package com.jmpdev.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by jacek on 10.01.16.
  */
+@RestController
 public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
 }
+
