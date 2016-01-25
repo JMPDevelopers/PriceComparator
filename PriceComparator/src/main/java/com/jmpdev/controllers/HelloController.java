3c09d3a4c3d00483b94afe2476a34bad32394a23
ja3ekja <jacekfeliksiak@gmail.com>
2016-01-26 00:30:56 +0100

    controller repository

--- PriceComparator/src/main/java/com/jmpdev/controllers/HelloController.java
+++ PriceComparator/src/main/java/com/jmpdev/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.jmpdev.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
     public String index() {
         return "Greetings from Spring Boot!";
     }
